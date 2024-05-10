0521fe00874735dd8335326a3673fac5ef9e5710
bluejynz <dany_falk@hotmai.com>
2024-05-10 15:16:04 -0300

    updating mob spawner

--- systems/mob_spawner.gd
+++ systems/mob_spawner.gd
@@ -1,12 +1,27 @@
 extends Node2D
 
-@onready var path_follow_2d: PathFollow2D = %Path2D/PathFollow2D
+@onready var path_follow_2d: PathFollow2D = %PathFollow2D
 
 @export var creatures: Array[PackedScene]
+@export var mobs_per_minute: float = 60
+var cooldown: float = 0
 
 func _process(delta: float) -> void:
-	pass
+	cooldown -= delta
+	if cooldown > 0: return
+	
+	var interval = 60 / mobs_per_minute
+	cooldown = interval
+	
+	spawn_creature()
+
+func spawn_creature() -> void:
+	var index = randi_range(0, creatures.size() - 1)
+	var creature_prefab = creatures[index]
+	var creature = creature_prefab.instantiate()
+	creature.global_position = get_point()
+	get_parent().add_child(creature)
 
 func get_point() -> Vector2:
 	path_follow_2d.progress_ratio = randf()
-	return path_follow_2d.position
+	return path_follow_2d.global_position
