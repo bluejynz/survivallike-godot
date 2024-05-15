91812536a250e3cd7c98b0c22a1da8be491d87c5
bluejynz <dany_falk@hotmai.com>
2024-05-14 21:42:02 -0300

    creating damage digits

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -5,6 +5,13 @@ extends Node2D
 @export var hit_damage: int = 1
 @export var death_prefab: PackedScene
 
+var damage_digit_prefab: PackedScene
+@onready var damage_digit_marker = $DamageDigitMarker
+
+func _ready():
+	damage_digit_prefab = preload("res://misc/damage_digit.tscn")
+	
+
 func damage(amount: int) -> void:
 	health -= amount
 	print("Inimigo tomou dano, vida: ", health)
@@ -14,6 +21,14 @@ func damage(amount: int) -> void:
 	tween.set_ease(Tween.EASE_IN)
 	tween.set_trans(Tween.TRANS_QUINT)
 	tween.tween_property(self, "modulate", Color.WHITE, .2)
+	
+	var damage_digit = damage_digit_prefab.instantiate()
+	damage_digit.value = amount
+	if damage_digit_marker:
+		damage_digit.global_position = damage_digit_marker.global_position
+	else:
+		damage_digit.global_position = global_position
+	get_parent().add_child(damage_digit)
 	
 	if health <= 0:
 		die()
